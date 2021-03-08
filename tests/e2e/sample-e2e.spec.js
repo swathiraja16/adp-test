fe0ed1594e7fad0a18fd113afd13e3f0aaab44ed
Raja Swathi <sr16e@my.fsu.edu>
2021-03-07 23:17:17 -0500

    e2e testing

--- tests/e2e/sample-e2e.spec.js
+++ tests/e2e/sample-e2e.spec.js
@@ -6,5 +6,40 @@ describe("My First Test", () => {
     it('Visits the home page', () => {
         cy.visit('http://localhost:9000')
       })
+
+    it('button click', () => {
+      cy.get('#startTask').click()
+    })
+
+    it('button click', () => {
+      cy.get('#endTask').click()
+    })
+
+    it('check document', () => {
+      cy.document()
+    })
+
+    it('button hover', () => {
+      cy.get('#startTask').trigger('mouseover')
+    })
+    
+
+    it('get method', () => {
+      cy.intercept('GET', '/get-task', {
+        statusCode: 200,
+        body: 'it worked!'
+      })
+    })
+
+    // it('post method', () => {
+    //   cy.request('POST', '/submit-task', {
+    //     'id':'17d80ba5-cace-44da-8f0d-ba20ecb145d4',
+    //     'result': 4975671346527996
+    //   })
+    //   .then((response) => {
+    //     // response.body is automatically serialized into JSON
+    //     expect(response.body).to.have.property('Correct') // true
+    //   })
+    // })
   });
   
